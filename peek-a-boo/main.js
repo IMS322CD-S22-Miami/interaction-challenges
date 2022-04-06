b5198dbd478c80cde21dc3a8e719fa658d724b2b
Eric Sheffield <ersheff@gmail.com>
2022-04-06 11:26:43 -0400

    1

---
+++ peek-a-boo/main.js
@@ -0,0 +1,6 @@
+let showHide = document.getElementById("show-hide");
+let navDrawer = document.getElementById("nav-drawer");
+
+showHide.addEventListener("click", () => {
+  navDrawer.classList.toggle("hide");
+})
